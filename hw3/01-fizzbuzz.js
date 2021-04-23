9f4f1614e2a96353ea2243e89fdcf2451a37945f
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-23 15:50:07 -0700

    complete 01

--- hw3/01-fizzbuzz.js
+++ hw3/01-fizzbuzz.js
@@ -1,5 +1,28 @@
+function divisible(n, x) {
+  result = n % x;
+  if (result === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function fizzbuzz() {
   // Add your code here
+  for (i = 1; i < 100; i++) {
+    var three = divisible(i, 3);
+    var five = divisible(i, 5);
+
+    if (three && five) {
+      console.log('fizzbuzz');
+    } else if (three) {
+      console.log('fizz');
+    } else if (five) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 fizzbuzz();
