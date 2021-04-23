b830f387e11f353b42363defeb56e17e9a8bbeed
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-23 16:53:59 -0700

    complete 02

--- hw3/02-coins.js
+++ hw3/02-coins.js
@@ -1,5 +1,58 @@
 let calculateChange = (input) => {
   // Add your code here
+
+  // start the output string
+  var output = "";
+  output = output + input + ' ==> ';
+
+  // check bounds
+  if (input > 10) {
+    return output + 'Error: input too large';
+  }
+
+  // struct for holding change values
+  var change = {
+    dollar: 0,
+    quarter: 0,
+    dime: 0,
+    nickel: 0,
+    penny: 0
+  }
+
+  // use cents so we don't worry about floating point complications
+  var cents = input * 100;
+
+  // calculate the change and save it to the struct
+  change.dollar = Math.floor(cents / 100);
+  cents = cents % 100;
+  change.quarter = Math.floor(cents / 25);
+  cents = cents % 25;
+  change.dime = Math.floor(cents / 10);
+  cents = cents % 10;
+  change.nickel = Math.floor(cents / 5);
+  cents = cents % 5;
+  change.penny = cents;
+
+
+
+  // build the output string using info from the struct
+  for (property in change) {
+    if (change[property] != 0) {
+      output = output + change[property] + ' ';
+      if (change[property] > 1 && property != 'penny') {
+        output = output + property + 's, ';
+      } else if (change[property] > 1 && property == 'penny') {
+        output = output + 'pennies ';
+      } else if (change[property] == 1 && property != 'penny') {
+        output = output + property + ', ';
+      } else {
+        output = output + property;
+      }
+    }
+  }
+
+  // return the output string
+  return output;
 };
 
 // Sample Test Cases
