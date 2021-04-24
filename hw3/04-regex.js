ff7e4e8df26dd8a2225585e87bd9ec8f7927220f
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-24 14:58:07 -0700

    04 complete, i think...

--- hw3/04-regex.js
+++ hw3/04-regex.js
@@ -1,1 +1,17 @@
 // Add your code here
+
+// create a callback for pressing 'enter' on the input field?
+document.querySelector('input').onchange = function () {
+    // extract the text input
+    var text = document.querySelector('input').value;
+
+    // create a regex that matches 'string' + '@' + 'string' + '.' + 'string'
+    let regex = /\S+@\S+\.\S+/;
+
+    // check for a match and print a message inside the <div> result element
+    if (regex.test(text)) {
+        document.querySelector('#result').innerHTML = '</br>' + 'Thank You. This is a valid email address.';
+    } else {
+        document.querySelector('#result').innerHTML = '</br>' + 'Error: Please enter a valid email address';
+    }
+};
