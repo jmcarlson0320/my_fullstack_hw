c649c72983205d9f46a1c3e6c0929038ca53e88e
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-24 13:36:07 -0700

    03 working

--- hw3/03-reverse.js
+++ hw3/03-reverse.js
@@ -1,3 +1,21 @@
 document.querySelector('#reverse').onclick = function () {
   // Add your code here
+
+  // get the number from the input field
+  var input = document.querySelector('#input').value;
+
+  // check that it is an 8 digit number
+  // if not, send alert and return
+  if (input.length != 8) {
+    alert('input must be an 8-digit number');
+    return;
+  }
+
+  // reverse the number
+  array = input.split("");    // convert to array
+  array.reverse();            // reverse the array
+  reversed = array.join("");  // convert back to string
+
+  // write the reversed number back to the input field
+  document.querySelector('#input').value = reversed;
 };
