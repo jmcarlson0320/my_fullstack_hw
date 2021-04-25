d0b9c83d96e812af919b2cc4165a5b6fbfce877c
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-24 18:56:51 -0700

    done..

--- hw3/05-form.js
+++ hw3/05-form.js
@@ -4,17 +4,30 @@ function onSubmit (event) {
   // needed in order to process the form input manually with javascript...
   event.preventDefault();
 
-  // see if this is being called
-  console.log('function called');
-
   // get fields from the form
   var form = document.querySelector('#form');
+
   var name = form.elements.name.value;
   var email = form.elements.email.value;
   var comments = form.elements.comments.value;
+  if (comments == '') {
+    comments = 'No feedback was submitted.';
+  }
 
-  // print them to the console
-  
+  var newsletter = '';
+  if (form.elements.checkbox.checked) {
+    newsletter = 'Yes, I would like to join the newsletter.'
+  } else {
+    newsletter = 'No, thank you.'
+  }
+
+  // print everything to the console
+  console.group('========= Form Submission =========');
+  console.log('Name: ' + name);
+  console.log('Email: ' + email);
+  console.log('Feedback: ' + comments);
+  console.log('Newsletter: ' + newsletter);
+  console.groupEnd();
 }
 
 // setup onSubmit as a callback function for the form submission event
