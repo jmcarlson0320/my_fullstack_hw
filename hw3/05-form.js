a0cb61e6482a1255583382d72d2e10d395331de0
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-24 18:25:07 -0700

    start of 05, works so far

--- hw3/05-form.js
+++ hw3/05-form.js
@@ -1,3 +1,22 @@
-function handleSubmit(event) {
+function onSubmit (event) {
   // Add your code here
+
+  // needed in order to process the form input manually with javascript...
+  event.preventDefault();
+
+  // see if this is being called
+  console.log('function called');
+
+  // get fields from the form
+  var form = document.querySelector('#form');
+  var name = form.elements.name.value;
+  var email = form.elements.email.value;
+  var comments = form.elements.comments.value;
+
+  // print them to the console
+  
 }
+
+// setup onSubmit as a callback function for the form submission event
+var myForm = document.querySelector('#form');
+myForm.addEventListener("submit", onSubmit);
