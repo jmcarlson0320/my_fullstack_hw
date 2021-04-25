da363db6692fe493e1284dbbfd836188da155350
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-24 19:57:05 -0700

    06 can get the data from the api

--- hw3/06-api.js
+++ hw3/06-api.js
@@ -1,7 +1,19 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
-let getData = (url) => {
+async function getData(url) {
   // Add your code here
+
+  // fetch the data from the url, store in json format
+  var response = await fetch('https://restcountries.eu/rest/v2/all');
+  var data = await response.json();
+
+  // loop through the data and print the name and population to the html
+  for (i = 0; i < data.length; i++) {
+    var country = data[i];
+    var name = country.name;
+    var population = country.population;
+    console.log(name + population);
+  }
 };
 
 getData(url);
