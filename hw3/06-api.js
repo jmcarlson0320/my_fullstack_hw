ff97a17bf04b652fc4472a3b5f34aa46357a6b48
Joshua Carlson <jmcarlson0320@gmail.com>
2021-04-24 21:06:37 -0700

    done!

--- hw3/06-api.js
+++ hw3/06-api.js
@@ -4,15 +4,29 @@ async function getData(url) {
   // Add your code here
 
   // fetch the data from the url, store in json format
-  var response = await fetch('https://restcountries.eu/rest/v2/all');
-  var data = await response.json();
+  try {
+    var response = await fetch(url);
+    var data = await response.json();
+  } catch (error) {
+    console.log('Error: request failed', error);
+    return;
+  }
 
   // loop through the data and print the name and population to the html
   for (i = 0; i < data.length; i++) {
     var country = data[i];
     var name = country.name;
     var population = country.population;
-    console.log(name + population);
+
+    // format population with commas
+    population = population.toLocaleString();
+
+    // create a new list item
+    var entry = document.createElement('li');
+    entry.innerText = name + ' - ' + population;
+
+    // add the list item to the list
+    document.getElementById('results').appendChild(entry);
   }
 };
 
