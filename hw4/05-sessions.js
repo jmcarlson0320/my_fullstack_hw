9e0cce9556a5cc773ba200bc3b45ec2c12186336
Joshua Carlson <jmcarlson0320@gmail.com>
2021-05-08 21:27:11 -0700

    05 works!!!!

--- hw4/05-sessions.js
+++ hw4/05-sessions.js
@@ -6,10 +6,39 @@ const port = process.env.PORT || 5000;
 // Add your code here
 
 // Use the express-session module
-app.use(/** ... */);
+app.use(session({
+  'store': new session.MemoryStore(),
+  'secret': 'a secret to sign the cookie',
+  'resave': false,
+  'saveUninitialized': false,
+  'cookie': {
+    'maxAge': 86400000
+  }
+}));
 
-app.get('/', (req, res) => {
+// this handles any url connected to /
+app.get('/*', (req, res) => {
   // Add your code here
+  res.status(200);
+  res.set({ 'Content-Type': 'text/plain' });
+
+  // start building the message
+  msg = `Currently on route: ${req.path}\n\n`;
+
+  // if no session, start a new "visited" list
+  if (req.session.visited === undefined) {
+    req.session.visited = [];
+    req.session.visited.push(req.path);
+    msg = msg + `Welcome to ${req.hostname}:${port}`
+  // If there already exists a session, print path and add this req to the list
+  } else {
+    msg = msg + `Previously visited:\n\n`;
+    req.session.visited.forEach(elem => {
+      msg = msg + elem + '\n';
+    });
+    req.session.visited.push(req.path);
+  }
+  res.send(msg);
 });
 
 app.listen(port, () => {
